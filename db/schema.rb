4ede8e08a246f1bb16b9de68956c15f3a9cf1933
Milind <7milind7@gmail.com>
2017-09-29 19:32:55 +0530

    models relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928105308) do
+ActiveRecord::Schema.define(version: 20170929133212) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(version: 20170928105308) do
     t.string "excerpt"
     t.string "location"
     t.integer "user_id"
+  end
+
+  create_table "articles_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.index ["article_id"], name: "index_articles_categories_on_article_id"
+    t.index ["category_id"], name: "index_articles_categories_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
