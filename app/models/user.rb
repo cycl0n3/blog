221b67cee2ab0fe7a35cba48b28805bc7297009f
Milind <7milind7@gmail.com>
2017-10-02 11:38:02 +0530

    changes in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates_uniqueness_of :email
+  validates_length_of :email, :within => 5..50
+
   has_one :profile
   has_many :articles, -> { order('published_at DESC, title ASC') },
            :dependent => :nullify
